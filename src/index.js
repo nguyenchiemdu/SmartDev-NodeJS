cfa92df6d40c5b9c84a75b7b758ea6377ca21afd
Dự Nguyễn Chiếm <nnguenchiemdu@gmail.com>
2021-12-26 11:02:05 +0700

    config port for deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const ejs = require('ejs')
 const path = require('path')
 const app = express()
 const morgan = require('morgan')
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 const route = require('./routes/index_route.js')
